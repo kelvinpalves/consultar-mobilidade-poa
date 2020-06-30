6f12983568efd812f5d735adeac54d7d79e874bc
Kelvin Alves <kalves@kopp.com.br>
2020-06-30 10:37:05 -0300

    testes unitários

--- README.md
+++ README.md
@@ -1,37 +1,30 @@
 # Consulta de Mobilidade Urbana - Porto Alegre / RS
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.6.
+Este projeto foi desenvolvido para consultar as linhas de ônibus, lotação e itinerários através da consulta da API do DataPoa.
 
 [![BCH compliance](https://bettercodehub.com/edge/badge/kelvinpalves/consultar-mobilidade-poa?branch=master)](https://bettercodehub.com/)
 
 # Dependências
 
-## Angular CLI
+As dependências abaixo são necessárias para executar o projeto:
+
+### Angular CLI
 npm install -g @angular/cli@9.0.7
 
-## Dependências do Projeto
+> O comando acima deve ser executado como super usuário.
+
+### Instalar módulos npm
 npm install
 
-## Development server
+## Executar
 
-Run `ng serve --aot` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
-
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+Para executar o sistema, acesse a raiz do projeto e execute o seguinte comando: `ng serve --aot`.
 
 ## Build
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+Para gerar a build do sistema, acessa a raiz do projeto e execute o seguinte comando: `ng build`. O build será armazenado no diretório `dist`. 
 
-## Running unit tests
+## Teste de unidade
 
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+Para executar os testes de unidade execute o comando: `ng test`.
 
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
--- src/app/consultar/components/consultar/consultar.component.spec.ts
+++ src/app/consultar/components/consultar/consultar.component.spec.ts
@@ -1,14 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'
+import { ConsultarService } from '../../service';
+import { ToastrModule } from 'ngx-toastr';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ConsultarComponent } from './consultar.component';
+import { ConsultarRoutes } from '../../consultar-routing.module';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
-describe('ConsultarComponent', () => {
+describe('Componente de Consulta de Linhas de Lotação e Ônibus', () => {
   let component: ConsultarComponent;
   let fixture: ComponentFixture<ConsultarComponent>;
 
+  let TITULO_ONIBUS: string = "Consultar linhas de ônibus";
+  let TITULO_LOTACAO: string = "Consultar linhas de lotação";
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ConsultarComponent ]
+      declarations: [ ConsultarComponent ],
+      imports: [
+        CommonModule, RouterModule, HttpClientModule, ReactiveFormsModule,
+        NgSelectModule, NgxDatatableModule,
+        ToastrModule.forRoot(),
+        RouterModule.forRoot(ConsultarRoutes)
+      ],
+      providers: [
+        ConsultarService
+      ]
     })
     .compileComponents();
   }));
@@ -19,7 +39,49 @@ describe('ConsultarComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Validar criação do componente.', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Validar título de acordo com a rota.', () => {
+    component.tipoConsulta = 'onibus';
+    component.definirTitulo();
+    expect(component.titulo).toEqual(TITULO_ONIBUS);
+
+    component.tipoConsulta = 'lotacao';
+    component.definirTitulo();
+    expect(component.titulo).toEqual(TITULO_LOTACAO);
+  });
+
+  it('Validar se linha está selecionada.', () => {
+    component.form.get('linha').setValue('53');
+    expect(component.linhaSelecionada()).toBeTrue();
+
+    component.form.get('linha').setValue(null);
+    expect(component.linhaSelecionada()).toBeFalse();
+  });
+
+  it('Validar se ponto está sendo criado.', () => {
+    let index = 2;
+    let ponto = { lat: 10, lng: 10 };
+
+    let esperado = {
+      ponto: 3, latitude: 10, longitude: 10,
+      linkGoogleMaps: `<a href="https://www.google.com/maps/?q=10,10" target="_">Visualizar no Mapa</a>`
+    };
+
+    let retorno = component.criarPonto(index, ponto);
+    expect(retorno).toEqual(esperado);
+
+    index = 10;
+    ponto = { lat: 350, lng: 430 };
+
+    esperado = {
+      ponto: 11, latitude: 350, longitude: 430,
+      linkGoogleMaps: `<a href="https://www.google.com/maps/?q=350,430" target="_">Visualizar no Mapa</a>`
+    };
+
+    retorno = component.criarPonto(index, ponto);
+    expect(retorno).toEqual(esperado);
   });
 });
